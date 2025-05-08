d2ed3a419abf84cc4154391e90b03c1ff15a664d
Rishthewizard <92958472+Rishthewizard@users.noreply.github.com>
2025-05-07 17:56:08 -0700

    Revert "underline test"
    This reverts commit 7df63dc99a2f7df6b0810bccd2b16b1c16275fd4.

--- site/src/jsMain/kotlin/io/github/rishthewizard/components/TopBar.kt
+++ site/src/jsMain/kotlin/io/github/rishthewizard/components/TopBar.kt
@@ -58,19 +58,16 @@ fun TopBar() {
                     "#",
                     item,
                     modifier =
-                        Modifier
-                            .styleModifier {
-                                color(Color("#F5E5CC"))
-                                fontSize(16.px)
-                                fontWeight("400")
-                                lineHeight(24.px)
-                                textDecoration("none") // Ensure no underline
-                            }.then(
-                                Modifier.styleModifier {
-                                    property("transition", "text-decoration 0.2s ease-in-out")
-                                    property(":hover", "text-decoration: underline")
-                                },
-                            ),
+                        Modifier.styleModifier {
+                            color(Color("#F5E5CC"))
+                            fontSize(16.px)
+                            fontWeight("400")
+                            lineHeight(24.px)
+                            textDecoration("none") // Ensure no underline
+                            textDecoration("none")
+                            property("transition", "text-decoration 0.2s ease-in-out")
+                            property("&:hover", "text-decoration: underline") // Add underline on hover
+                        },
                 )
             }
         }
